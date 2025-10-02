39b1770e53bc0c816331210efad8e27f4fc49dae
student nikhildemo <nikhilshinde2257@gmail.com>
2025-10-03 00:48:42 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: `https://jobmitra-frontend.onrender.com/`,
+  origin: `https://jobmitra-frontend.onrender.com`,
   credentials: true,
 };
 app.use(cors(corsOptions));
