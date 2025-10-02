03416e9b522e03cd26012dcb04dc3cb9026405b9
student nikhildemo <nikhilshinde2257@gmail.com>
2025-10-03 02:21:20 +0530

    main.jsx updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+
 import App from './App.jsx'
 import { Toaster } from './components/ui/sonner'
 import { Provider } from 'react-redux'
@@ -13,9 +13,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
      <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+      
        <App />
-      </BrowserRouter>
+      
 
      </PersistGate>
 
