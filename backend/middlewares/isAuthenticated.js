0edd5e61bc1e2bd0f9c5d9b2fcec56a142ed46b7
student nikhildemo <nikhilshinde2257@gmail.com>
2025-10-03 13:08:21 +0530

    updated authentication

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -1,8 +1,17 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 const isAuthenticated = async (req, res, next) => {
   try {
+    console.log("REQ AUTH HEADER:", req.headers.authorization);
+
     const authHeader = req.headers.authorization;
+    // return  res.json({
+    // //   status:200,
+    // //   message:authHeader
+
+    // //  });
 
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res.status(401).json({
@@ -12,6 +21,7 @@ const isAuthenticated = async (req, res, next) => {
     }
 
     const token = authHeader.split(" ")[1];
+    console.log(token);
     const decode = jwt.verify(token, process.env.SECRET_KEY);
 
     if (!decode) {
@@ -31,5 +41,34 @@ const isAuthenticated = async (req, res, next) => {
     });
   }
 };
-
 export default isAuthenticated;
+// const jwt = require('jsonwebtoken');
+// //require('dotenv').config();
+
+// const checkAuthorization = (req,res,next)=>{
+//   const token = req.headers.authorization;
+//     if(token){
+//       try{
+//         const decoded = jwt.verify(token.replace('Bearer ','').process.env.SECRET);
+
+//         req.id = decoded.id;
+//         req.username = decoded.username;
+//         req.privilege = decoded.privilege;
+//         req.name = decoded.name;
+//         next();
+//       }catch(error){
+//         res.json({
+//           status:401,
+//           message:'Unauthorized access denied'
+//         });
+//       }
+//     }else{
+//       res.json({
+//         status:401,
+//         message:'unauthorized access denied'
+//       });
+//     }
+
+// }
+
+// module.exports = {checkAuthorization};
